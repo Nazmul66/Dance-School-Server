8b685536dc0ddb368e6582aaca44fde7da4db144
Nazmul66 <hnazmul748@gmail.com>
2023-06-09 09:04:23 -0700

    coursr cart has benn store

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-tfmdlv0-shard-00-00.rnkzyeb.mongodb.net:27017,ac-tfmdlv0-shard-00-01.rnkzyeb.mongodb.net:27017,ac-tfmdlv0-shard-00-02.rnkzyeb.mongodb.net:27017/?ssl=true&replicaSet=atlas-8cmvoo-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const CourseCollection = client.db("Dance_School").collection("Cart");
+
+    app.post("/course", async(req, res) =>{
+        const body = req.body;
+        const result = await CourseCollection.insertOne(body)
+        res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
